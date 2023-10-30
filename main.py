7d25efdc8f1fa8a1ede9a72d6fa5130e07dde61b
Alexey Snytnikov <snytav@gmail.com>
2023-10-30 22:41:24 +0200

    LSTM class in a file

--- main.py
+++ main.py
@@ -85,21 +85,7 @@ num_layers = 2
 output_dim = 1
 num_epochs = 100
 
-class LSTM(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
-        super(LSTM, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.num_layers = num_layers
-
-        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_dim, output_dim)
-
-    def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_()
-        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
-        out = self.fc(out[:, -1, :])
-        return out
+from LSTM_net import LSTM
 
 model = LSTM(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
 criterion = torch.nn.MSELoss(reduction='mean')
@@ -168,6 +154,7 @@ trainScore = math.sqrt(mean_squared_error(y_train[:,0], y_train_pred[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(y_test[:,0], y_test_pred[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
+# Test Score: 257.40 RMSE
 lstm.append(trainScore)
 lstm.append(testScore)
 lstm.append(training_time)
